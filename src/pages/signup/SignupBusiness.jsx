bcb2eb2fc39192ea1b29a8676d62cde6416ca345
BTC001B <btc001b.btcglobal@gmail.com>
2026-09-23 15:50:51 +0530

    refactor: use VITE_API_BASE environment variable for API_BASE in SignupBusiness

--- src/pages/signup/SignupBusiness.jsx
+++ src/pages/signup/SignupBusiness.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = import.meta.env.VITE_API_BASE;
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useAppContext } from '../../context/AppContext';
